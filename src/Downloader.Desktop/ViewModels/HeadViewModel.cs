bf8f8d11f1dc9d29cfb77d732d0f180440e9c26e
bezzad <behzad.khosravifar@gmail.com>
2024-09-03 00:40:24 +0330

    changed app style as user-friendly

--- src/Downloader.Desktop/ViewModels/HeadViewModel.cs
+++ src/Downloader.Desktop/ViewModels/HeadViewModel.cs
@@ -15,6 +15,11 @@ public class HeadViewModel : ViewModelBase
 
     public ICommand AddUrlCommand { get; }
     public ICommand RemoveItemCommand { get; }
+    public ICommand StopAllCommand { get; }
+    public ICommand StopItemCommand { get; }
+    public ICommand StartItemCommand { get; }
+    public ICommand ShowOptionsViewCommand { get; }
+    public string DownloadUrl { get; set; }
 
     /// <summary>
     /// Gets or sets a list of Files
--- src/Downloader.Desktop/ViewModels/MainViewModel.cs
+++ src/Downloader.Desktop/ViewModels/MainViewModel.cs
@@ -1,21 +1,40 @@
 ﻿using Downloader.Desktop.Services;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Reactive.Concurrency;
 using ReactiveUI;
 using System.Threading;
+using System.Windows.Input;
 
 namespace Downloader.Desktop.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
     public DownloadsViewModel Downloads { get; private set; }
-    public HeadViewModel Head { get; } = new HeadViewModel();
-
+    // public HeadViewModel Head { get; } = new HeadViewModel();
+    public ICommand AddUrlCommand { get; }
+    public ICommand RemoveItemCommand { get; }
+    public ICommand StopItemCommand { get; }
+    public ICommand StartItemCommand { get; }
+    public ICommand ShowOptionsViewCommand { get; }
+    public string DownloadUrl { get; set; }
+    
+    /// <summary>
+    /// Gets or sets a list of Files
+    /// </summary>
+    private IEnumerable<string>? _SelectedFiles;
+    private IEnumerable<string>? SelectedFiles
+    {
+        get { return _SelectedFiles; }
+        set { this.RaiseAndSetIfChanged(ref _SelectedFiles, value); }
+    }
+    
     public MainViewModel()
     {
         RxApp.MainThreadScheduler.ScheduleAsync(InitMainViewModelAsync);
+        AddUrlCommand = ReactiveCommand.CreateFromTask(SelectFilesAsync);
     }
 
     private async Task InitMainViewModelAsync(IScheduler scheduler, CancellationToken ct)
@@ -35,5 +54,13 @@ public class MainViewModel : ViewModelBase
                 Downloads.DownloadItems.Add(new(item));
             }
         }
+    }
+
+    /// <summary>
+    /// A command used to select some files
+    /// </summary>
+    private async Task SelectFilesAsync()
+    {
+        SelectedFiles = await this.OpenFileDialogAsync("Hello Avalonia");
     }
 }
