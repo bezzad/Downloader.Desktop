735b32bd257b0952ad420f65f538f5ca1a95b97e
bezzad <behzad.khosravifar@gmail.com>
2026-05-30 09:55:23 +0330

    fix warnings about update to .Net 10

--- src/Downloader.Desktop/Program.cs
+++ src/Downloader.Desktop/Program.cs
@@ -5,7 +5,7 @@ using Avalonia.Controls;
 
 namespace Downloader.Desktop;
 
-class Program
+static class Program
 {
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
@@ -17,7 +17,7 @@ class Program
     }
 
     // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
+    private static AppBuilder BuildAvaloniaApp()
     {
         return AppBuilder.Configure<App>()
                .UsePlatformDetect()
